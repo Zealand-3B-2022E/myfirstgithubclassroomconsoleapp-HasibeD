da20df6df311983ca43bdf1f76ed5d00c6832fe8
Hasibe Dönmez <hasi0048@easjdk.onmicrosoft.com>
2022-09-05 12:41:54 +0200

    hej

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public int MyBook { get; set; }
+        public string MyBookName { get; set; }
+        public int MyBookAuthor { get; set; }
+
+
+    }
+    
+    
+    
+}
